884d3831cefa01801f6d14da3787df131293c8b0
abdul <abdul.azis@ionnex.com>
2020-06-22 19:00:25 +0700

    add imge module

--- config/app.php
+++ config/app.php
@@ -160,7 +160,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\HelperServiceProvider::class,
-        UxWeb\SweetAlert\SweetAlertServiceProvider::class,
     ],
 
     /*
@@ -210,7 +209,6 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Helper' => App\Helpers\Helper::class,
-        'Alert' => UxWeb\SweetAlert\SweetAlert::class,
 
     ],
 
