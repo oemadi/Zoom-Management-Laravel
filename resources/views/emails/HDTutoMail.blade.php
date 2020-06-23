e92cfb93af00266531d428ae7745c70d14317c93
abdul <abdul.azis@ionnex.com>
2020-06-23 13:33:45 +0700

    add imge module

--- resources/views/emails/HDTutoMail.blade.php
+++ resources/views/emails/HDTutoMail.blade.php
@@ -13,7 +13,7 @@
     <h1>Terimakasih anda telah bergabung </h1>
     <p>haloo {{ $user->name }}</p>
     <h1>Mohon klik link di bawah ini !</h1>
-<a href="http://localhost/zoomweb/user/check/{{$user->id}}">
+    <a href="https://automataseminar.com/zoom/user/check/{{$user->id}}">
 
 </body>
 
