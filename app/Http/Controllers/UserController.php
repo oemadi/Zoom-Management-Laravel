7ae048d5be3c10c26e795d652d88ea19a4f2bce3
abdul <abdul.azis@ionnex.com>
2020-06-24 00:03:30 +0700

    add login

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,7 +3,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Event;
-use App\Models\user;
+use App\Models\User;
 use Session;
 
 class UserController extends Controller
